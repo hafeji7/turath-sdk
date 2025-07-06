20706f6aaa24599d478055bd189485edad40b498
hafeji7 <mohammedhafeji786@gmail.com>
2025-07-06 17:23:13 -0400

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,11 @@ export * from './api';
 export * from './types/api/author';
 export * from './types/api/bookFile';
 export * from './types/index';
+
+export default {
+  async fetch(request: Request) {
+    return new Response("Turath SDK is live!", {
+      headers: { "content-type": "text/plain" },
+    });
+  }
+};
